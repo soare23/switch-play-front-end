745e0063e6a9f8f4fdb73d18a628589ee5310270
soare23 <toma.mihai.sorin23@gmail.com>
2021-01-13 15:16:13 +0200

    small modifications

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,29 +7,25 @@ function Login() {
     email: '',
     password: '',
   });
-  //   const [loggedIn, setLoggedIn] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
 
-  function handleSubmit() {
+  function handleSubmit(e) {
+    e.preventDefault();
     axios
       .post(`/api/check-if-user`, user)
       .then((response) => {
         console.log(response);
         if (response.status === 200) {
-          //   setLoggedIn(true);
-          window.location.href('http://localhost:3000/');
-          //   const token = response.session.user;
-          //   console.log(token);
-          console.log('success');
+          setLoggedIn(true);
+          window.location.href = 'http://localhost:3000/prehome';
         }
       })
       .catch((err) => {
         console.log(err);
         if (err.response.status === 401) {
           console.log('wrong password');
-          window.location.href('http://localhost:3000/');
         } else if (err.response.status === 409) {
           console.log('wrong email');
-          window.location.href('http://localhost:3000/');
         } else {
           console.log(err);
         }
