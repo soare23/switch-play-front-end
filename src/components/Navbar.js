1b7bbec3079eb7097cac4486c1764fe721a53d04
soare23 <toma.mihai.sorin23@gmail.com>
2021-01-13 14:11:06 +0200

    added login on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
 function Navbar(props) {
-
-
-    return (
-        <div style={{"backgroundColor" : "#e8f2f6"}}>
-            <div className="nav justify-content-start">
-            <a className="nav-link" href="/">Home</a>
-            <a className="nav-link" href={"/register"}>Register</a>
-            </div>
-        </div>
-    );
+  return (
+    <div style={{ backgroundColor: '#e8f2f6' }}>
+      <div className="nav justify-content-start">
+        <a className="nav-link" href="/">
+          Home
+        </a>
+        <a className="nav-link" href={'/register'}>
+          Register
+        </a>
+        <a className="nav-link" href={'/login'}>
+          Login
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
