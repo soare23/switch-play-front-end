724d34be3b0a2e7b074ce5dbd3b75dc12408ee84
soare23 <toma.mihai.sorin23@gmail.com>
2021-01-14 13:51:21 +0200

    added routes to new componenets

--- src/App.js
+++ src/App.js
@@ -6,18 +6,21 @@ import UserProfile from './components/UserProfile';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import './App.css';
-import AddGame from "./components/AddGame";
+import AddGame from './components/AddGame';
+import SearchGame from './components/SearchGame';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      
+
       <Router>
         <Route exact path={'/register'} component={Register} />
         <Route exact path={'/login'} component={Login} />
         <Route exact path={'/homepage'} component={HomePage} />
         <Route exact path={'/profile'} component={UserProfile} />
+        <Route exact path={'/add-game'} component={AddGame} />
+        <Route exact path={'/search-game'} component={SearchGame} />
       </Router>
     </div>
   );
