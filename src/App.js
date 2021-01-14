c45f6667f1beabc9711f1c77a2d50faad2dd9c4a
soare23 <toma.mihai.sorin23@gmail.com>
2021-01-14 13:13:11 +0200

    added route to user profile

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Register from './components/Register';
 import Login from './components/Login';
 import HomePage from './components/HomePage';
+import UserProfile from './components/UserProfile';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import './App.css';
@@ -15,6 +16,7 @@ function App() {
         <Route exact path={'/register'} component={Register} />
         <Route exact path={'/login'} component={Login} />
         <Route exact path={'/homepage'} component={HomePage} />
+        <Route exact path={'/profile'} component={UserProfile} />
       </Router>
     </div>
   );
