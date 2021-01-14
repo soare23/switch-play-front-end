67bd7390aa9ec313f092b155e13aeaa972fd7558
soare23 <toma.mihai.sorin23@gmail.com>
2021-01-14 13:14:04 +0200

    added profile route

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ function Navbar(props) {
   return (
     <div style={{ backgroundColor: '#e8f2f6' }}>
       <div className="nav justify-content-start">
-        <a className="nav-link" href="/homepage">
+        <a className="nav-link" href={'/homepage'}>
           Home
         </a>
         <a className="nav-link" href={'/register'}>
@@ -13,6 +13,11 @@ function Navbar(props) {
         <a className="nav-link" href={'/login'}>
           Login
         </a>
+        <div>
+          <a className="nav-link" href={'/profile'}>
+            Profile
+          </a>
+        </div>
       </div>
     </div>
   );
