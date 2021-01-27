343c3b182e033da5c2b22d93a9b38e0b9b55a559
soare23 <toma.mihai.sorin23@gmail.com>
2021-01-27 13:31:03 +0200

    design tweaks

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import '../HomePage.css';
+import { UserContext } from './UserContext';
 
 function HomePage() {
+  const userDetails = useContext(UserContext);
+
   return (
     <div>
       <div className="card-container">
-        <Link to="/add-game">
-          <div className="card-home-page card-add"></div>
-        </Link>
+        {userDetails !== undefined ? (
+          <Link to="/add-game">
+            <div className="card-home-page card-add"></div>
+          </Link>
+        ) : (
+          <Link to="/login">
+            <div className="card-home-page card-add"></div>
+          </Link>
+        )}
 
         <Link to="/search-game">
           <div className="card-home-page card-search"></div>
